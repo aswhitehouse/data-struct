85c68383d8b661e86bf3069d6e515f5782335a8b
Andrew Whitehouse <andrewswhitehouse@gmail.com>
2023-07-18 09:40:14 +1000

    First

---
+++ BootStrapDs.js
@@ -0,0 +1,35 @@
+//Stack
+// Stacks are LIFO
+// Uses = Function call/return, E.G. executing functions creates it's stack frame and pushes the SF onto the stack
+// Storing variable data
+// Undo/Redo - Pop the last action from the stack and so-on
+// Browser History - As above
+// (DFS) - Deapth First Search algorithms, keeping track of the visited nodes and the current
+
+// COMMON METHODS:
+// pop - remove the top element and return it
+// push - push a new element into the top of the stack
+// peek - look at the top element but don't remove it
+// size - the size of the stack
+// isEmpty - is the stack empty?
+
+class Stack {
+    size;
+
+    size() {}
+
+    pop() {}
+
+    push(value) {}
+
+    peek() {}
+
+    isEmpty() {}
+}
+
+class Node {
+    // value;
+    // previous;
+    // next;
+}
+
